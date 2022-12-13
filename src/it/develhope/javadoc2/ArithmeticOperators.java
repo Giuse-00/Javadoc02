473a4829b0a20d32139e1d7db11abde9026ed5bd
Giuseppe Iacono <giuseiacono10@gmail.com>
2022-12-13 13:02:29 +0100

    Iacono esercizio Javadoc 02

---
+++ src/it/develhope/javadoc2/ArithmeticOperators.java
@@ -0,0 +1,47 @@
+package it.develhope.javadoc2;
+
+/**
+ * @author Giuseppe Iacono
+ * class ArithmeticOperators that represent 4 methods
+ */
+public class ArithmeticOperators {
+    /**
+     * method that sum num1 and num2
+     * @param num1 is the first int
+     * @param num2 is the second int
+     * @return int (num1+num2)
+     */
+    public int sum(int num1, int num2){
+        return num1 + num2;
+    }
+
+    /**
+     * method that makes the difference between num1 and num2
+     * @param num1 is the first int
+     * @param num2 is the second int
+     * @return int (num1-num2)
+     */
+    public int sub(int num1, int num2){
+        return num1 - num2;
+    }
+
+    /**
+     * method that multiplies num1 and num2
+     * @param num1 is the first int
+     * @param num2 is the second int
+     * @return int (num1*num2)
+     */
+    public int mul(int num1, int num2){
+        return num1 * num2;
+    }
+
+    /**
+     * method that divide num1 and num2
+     * @param num1 is the first int
+     * @param num2 is the second int
+     * @return int (num1/num2)
+     */
+    public int div(int num1, int num2){
+        return num1 / num2;
+    }
+}
---
+++ src/it/develhope/javadoc2/Tester.java
@@ -0,0 +1,19 @@
+package it.develhope.javadoc2;
+
+/**
+ * @author Giuseppe Iacono
+ * class Tester
+ */
+public class Tester {
+
+    public static void main(String[] args) {
+
+        ArithmeticOperators maths = new ArithmeticOperators();
+
+        maths.sum(4,6);
+        maths.sub(8,2);
+        maths.mul(7,2);
+        maths.div(5,2);
+
+    }
+}
