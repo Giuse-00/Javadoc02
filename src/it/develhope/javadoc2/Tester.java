048a46e6b31efc30998caa5d7dc6d820fd143c37
Giuseppe Iacono <giuseiacono10@gmail.com>
2022-12-27 13:35:18 +0100

    Iacono esercizio Javadoc 02 corretto

--- src/it/develhope/javadoc2/Tester.java
+++ src/it/develhope/javadoc2/Tester.java
@@ -10,10 +10,10 @@ public class Tester {
 
         ArithmeticOperators maths = new ArithmeticOperators();
 
-        maths.sum(4,6);
-        maths.sub(8,2);
-        maths.mul(7,2);
-        maths.div(5,2);
+        System.out.println("The sum is: " + maths.sum(4,6));
+        System.out.println("The difference is: " + maths.sub(8,2));
+        System.out.println("The multiplication is: " + maths.mul(7,2));
+        System.out.println("The division is: " + maths.div(4,2));
 
     }
 }
